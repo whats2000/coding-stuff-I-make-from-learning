ab879ab9f64d99ae560bd70ccc0c8eac2928d5e7
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-04-10 02:10:29 +0800

    Add files via upload

---
+++ pythons/預測回歸.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+n = int(input('請輸入矩陣數量 : '))
+X = []
+for i in range(n):
+    X.append([float(x) for x in input('請輸入矩陣').split()])
+y = [int(x) for x in input('請輸入判定布林值(0/1) : ').split()]
+datapoint = [float(x) for x in input('請輸入預測矩陣 : ').split()]
+
+model = LogisticRegression()
+model.fit(X, y)
+
+pre_datapoint = model.predict(np.array(datapoint).reshape(1, -1))
+
+print('預測布林值為 : ', end='')
+print(int(pre_datapoint))
