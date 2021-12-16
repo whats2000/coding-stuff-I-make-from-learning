9fd9072aba9200c3998cc170723fb145505bca00
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-12-16 10:56:10 +0800

    Update 隨機變數.cpp

--- C++-projects/隨機變數.cpp
+++ C++-projects/隨機變數.cpp
@@ -11,7 +11,7 @@ int main()
  auto seed = std::time(nullptr) ^ std::random_device()(); // 確保 random_device 沒正常工作下依然能使用
  std::mt19937 gen(seed);
 
- auto randfun = std::bind(dis, gen); //將引擎和分佈綁在一起變成新函數 randfun
+ auto randfun = std::bind(dis, gen); // 將引擎和分佈綁在一起變成新函數 randfun
 
- std::cout << randfun() << ' ' << randfun() << ' ' << randfun() << std::endl; //可以直接重複呼叫
+ std::cout << randfun() << ' ' << randfun() << ' ' << randfun() << std::endl; // 可以直接重複呼叫
 }
