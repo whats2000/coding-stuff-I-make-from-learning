3d221d80a413c147f90aeca802add6992e57f6fd
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-12-16 10:52:02 +0800

    Update 隨機變數.cpp

--- C++-projects/隨機變數.cpp
+++ C++-projects/隨機變數.cpp
@@ -1,15 +1,17 @@
 #include <functional>
 #include <random>
+#include <chrono>
 #include <iostream>
-using namespace std;
 
 int main()
 {
-	std::random_device rd;						 // 種子產生器
-	std::mt19937 gen = std::mt19937(rd());		 //使用mt19937引擎
-	std::uniform_real_distribution<> dis(-1, 1); //使用平均分佈
+ std::random_device rd;                       // 種子產生器
+ std::uniform_real_distribution<> dis(-1, 1); // 使用平均分佈
 
-	auto randfun = std::bind(dis, gen); //將引擎和分佈綁在一起變成新函數 randfun
+ auto seed = std::time(nullptr) ^ std::random_device()(); // 確保 random_device 沒正常工作下依然能使用
+ std::mt19937 gen(seed);
 
-	cout << randfun() << ' ' << randfun() << ' ' << randfun() << endl; //可以直接重複呼叫
+ auto randfun = std::bind(dis, gen); //將引擎和分佈綁在一起變成新函數 randfun
+
+ std::cout << randfun() << ' ' << randfun() << ' ' << randfun() << std::endl; //可以直接重複呼叫
 }
