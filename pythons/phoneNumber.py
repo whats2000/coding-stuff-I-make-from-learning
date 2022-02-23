ca0c9073953fd887e60a89b0fc463ee201e77ddc
whats2000 <60466660+whats2000@users.noreply.github.com>
2022-02-23 14:18:24 +0800

    format code

--- pythons/phoneNumber.py
+++ pythons/phoneNumber.py
@@ -1,8 +1,9 @@
 import re
-#your code goes here
+
+# your code goes here
 rawPhoneNum = r"^(1|8|9)[0-9]{7,7}$"
 
-if re.match(rawPhoneNum,input()):
-	print("valid")
+if re.match(rawPhoneNum, input()):
+    print("valid")
 else:
-	print("Invalid")
+    print("Invalid")
--- pythons/圖像函數.py
+++ pythons/圖像函數.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
-#Plotting the adjusted line over the scattered values
-y = np.linspace(0,80,100)                     #Evenly spaced points (100) in a given interval (from 0 to 80)
-x = (0.01549065*y + 0.51037152)/0.01615949    #X value taken from the line ecuation
-plt.plot(x, y, '-b', label='')                #Ploting the line
+# Plotting the adjusted line over the scattered values
+y = np.linspace(0, 80, 100)  # Evenly spaced points (100) in a given interval (from 0 to 80)
+x = (0.01549065 * y + 0.51037152) / 0.01615949  # X value taken from the line ecuation
+plt.plot(x, y, '-b', label='')  # Ploting the line
 plt.xlabel('Fare')
 plt.ylabel('Age')
-plt.scatter(df['Fare'], df['Age'], c=df['Survived'])  #plt.scatter(xAxis,yAxis,classes)
-plt.grid()                                    #Grid over plotted image
+plt.scatter(df['Fare'], df['Age'], c=df['Survived'])  # plt.scatter(xAxis,yAxis,classes)
+plt.grid()  # Grid over plotted image
 plt.show()
