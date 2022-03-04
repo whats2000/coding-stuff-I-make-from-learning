811ebfa6089542dc3a3537712142c5da1f42ff6d
whats2000 <60466660+whats2000@users.noreply.github.com>
2022-03-04 16:40:12 +0800

    Rename file to printCircle.c

--- C/prat1/fabonacci.c
+++ C/prat1/fabonacci.c
@@ -3,12 +3,13 @@
 int main(int argc, char* argv) {
     int num = 0;       // num = input arguments
     int a = 1, b = 1;  // a, b = template data
+    int i = 0;
 
     // input number
     scanf("%d", &num);
 
     // 將地一次及第二次獨立計算此外，其餘計算皆為將兩者合存於a後a, b交換
-    for (int i = 1; i <= num; i++) {
+    for (i = 1; i <= num; i++) {
         if (i == 1)
             printf("F1 = 1\n");
         else if (i == 2)
--- C/prat1/printCircle.c
+++ C/prat1/printCircle.c
@@ -2,14 +2,15 @@
 
 int main(int argc, char* argv) {
     int radius = 0;  // number of circle radius
+    int i = 0, j = 0;
 
     // input radius
     scanf("%d", &radius);
 
     // draw circle
     // 由i, j表達座標，藉由正負半徑確認圓心，再利用畢氏定理判定該位置是否在圓內
-    for (int i = -radius; i <= radius; i++) {
-        for (int j = -radius; j <= radius; j++)
+    for (i = -radius; i <= radius; i++) {
+        for (j = -radius; j <= radius; j++)
             if (i * i + j * j <= radius * radius)
                 printf("*");
             else
