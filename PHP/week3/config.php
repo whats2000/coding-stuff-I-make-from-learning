766b2ec136ca15ba4bb82f62d119f532cffba13d
whats2000 <whats2000mc@gmail.com>
2022-03-15 21:02:41 +0800

    Update config.php

--- PHP/week3/config.php
+++ PHP/week3/config.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect("localhost", "whats2000_", "123456789", "php_couse");
+    $link = mysqli_connect("localhost", "whats2000_", "123456789", "php_course");
     
     if (!$link) {
         printf("Could not connect : %s\n", mysqli_connect_error());
