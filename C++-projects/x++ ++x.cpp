869b673027175339d5ea3882ae3b571c8670009e
whats2000 <60466660+whats2000@users.noreply.github.com>
2022-05-25 14:48:35 +0800

    Remove some file

--- C++-projects/x++ ++x.cpp
+++ C++-projects/x++ ++x.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
 	int x = 5;
 	int y = 5;
-	cout << x++ * ++x << endl;
+	cout << x++ * (++x) << endl;
 	cout << ++y * y++ << endl;
 	cout << "x=" << x << endl;
 	cout << "y=" << y;
--- C++-projects/x_y.cpp
+++ C++-projects/x_y.cpp
@@ -1,14 +1,12 @@
-/* xy¹Bºâ */
+/* xyï¿½Bï¿½ï¿½ */
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-	int x=3,y=4;
-	float a=5,b=3;
-	printf("ans=%d\n",x+y);
-	system("Pause");
-	printf("%.0f/%.0f=%1.2f\n",a,b,a/b);
-	system("Pause");
-	return 0;
+int main(void) {
+    int x = 3, y = 4;
+    float a = 5, b = 3;
+    printf("ans=%d\n", x + y);
+    system("Pause");
+    printf("%.0f/%.0f=%1.2f\n", a, b, a / b);
+    system("Pause");
+    return 0;
 }
-
