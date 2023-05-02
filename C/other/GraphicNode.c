9e0cdcbb98c5c7b8585c76491891e0571f4874d7
whats2000 <whats2000mc@gmail.com>
2023-05-02 09:02:51 +0800

    Update GraphicNode.c

--- C/other/GraphicNode.c
+++ C/other/GraphicNode.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,19 +6,22 @@
 
 typedef struct node {
     int vertex;
+    int weight;
     struct node* link;
 } GraphNode;
 
-void add_edge(GraphNode** graph, int u, int v) {
+void add_edge(GraphNode** graph, int u, int v, int weight) {
     // 增加正向邊
     GraphNode* new_node = (GraphNode*)malloc(sizeof(GraphNode));
     new_node->vertex = v;
+    new_node->weight = weight;
     new_node->link = graph[u];
     graph[u] = new_node;
 
     // 增加反向邊
     GraphNode* reverse_node = (GraphNode*)malloc(sizeof(GraphNode));
     reverse_node->vertex = u;
+    reverse_node->weight = weight;
     reverse_node->link = graph[v];
     graph[v] = reverse_node;
 }
@@ -27,7 +31,7 @@ void print_graph(GraphNode** graph, int vertex_in_use) {
         printf("Vertex %d: ", i);
         GraphNode* current_node = graph[i];
         while (current_node != NULL) {
-            printf("%d ", current_node->vertex);
+            printf("(%d, %d) ", current_node->vertex, current_node->weight);
             current_node = current_node->link;
         }
         printf("\n");
@@ -38,24 +42,24 @@ int main() {
     GraphNode* graph[MAX_VERTICES] = {NULL};
     int vertex_in_use = 0;
 
-    add_edge(graph, 0, 1);
-    add_edge(graph, 0, 2);
-    add_edge(graph, 0, 7);
+    add_edge(graph, 0, 1, 16);
+    add_edge(graph, 0, 2, 13);
+    add_edge(graph, 0, 7, 27);
 
-    add_edge(graph, 1, 2);
+    add_edge(graph, 1, 2, 11);
 
-    add_edge(graph, 2, 7);
-    add_edge(graph, 2, 3);
+    add_edge(graph, 2, 3, 20);
+    add_edge(graph, 2, 7, 6);
 
-    add_edge(graph, 3, 4);
-    add_edge(graph, 3, 5);
-    add_edge(graph, 3, 7);
+    add_edge(graph, 3, 4, 9);
+    add_edge(graph, 3, 5, 18);
+    add_edge(graph, 3, 7, 19);
 
-    add_edge(graph, 4, 5);
+    add_edge(graph, 4, 5, 14);
 
-    add_edge(graph, 5, 6);
+    add_edge(graph, 5, 6, 26);
 
-    add_edge(graph, 6, 7);
+    add_edge(graph, 6, 7, 10);
     /*
     1 ----
     |     |
